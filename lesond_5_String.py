3a01019a85982865a0ecdfafe35c495627c1241c
Denis <kotisden@gmail.com>
2017-10-30 13:07:13 +0200

    Add lesond_5_String.py

---
+++ lesond_5_String.py
@@ -0,0 +1,20 @@
+name = "kotys denis"
+
+print(name.title()) #головні букви будуть великі
+print(name.upper()) #всі букви будуть великі
+print(name.lower()) #всі букви маленькі
+
+print("Mesenger :\n\tMesenger1\n\tMesenger2\n\tMesenger3\nEND")
+
+print("----------------------\n\n")
+a = "....... dadya denis .........."
+print(a)
+print(a.strip()) #стирає пробіли
+print(a.strip('.'))
+
+b =  "///...  Dadya denis  ......//"
+
+b = b.strip('/')
+b = b.strip(".")
+b = b.strip()
+print(b.title())
